81e3821d886eee860d2c20c87b19551215844f74
Balu <balupatil4815@gmail.com>
2023-03-05 21:24:27 +0530

    Processing

---
+++ booking-app/backend/routes/user-routes.js
@@ -0,0 +1,14 @@
+const express = require ("express");
+const { deleteUser, getAllUser, signup, updateUser, login, getBookingsofUser } = require ("../controllers/user-controller");
+const userRouter = express.Router();
+
+
+userRouter.get("/", getAllUser);
+userRouter.post("/signup", signup);
+userRouter.put("/:id", updateUser);
+userRouter.delete("/:id", deleteUser);
+userRouter.post("/login", login);
+userRouter.get("bookings/:id", getBookingsofUser); 
+
+
+module.exports = userRouter;      
